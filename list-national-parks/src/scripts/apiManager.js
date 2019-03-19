5af528f6958a6dae1758c44f509513a7fd5a63f6
Michael Yankura <myankura210@gmail.com>
2019-03-18 20:57:49 -0500

    added comments to remind myself what the hell i did.

--- list-national-parks/src/scripts/apiManager.js
+++ list-national-parks/src/scripts/apiManager.js
@@ -1,16 +1,16 @@
 console.log("apiManager.js");
 
 
-
-const getParks = (value) => {
+//GET request
+const getParks = () => {
     return fetch("http://localhost:9099/parks")
         .then(response => response.json())
 }
 
 getParks()
-    .then(parsedValue => {
-        console.log(parsedValue)
-        for (let i = 0; i < parsedValue.length; i++) {
+    .then(natlParks => {
+        console.log(natlParks)
+        for (let i = 0; i < natlParks.length; i++) {
             let articleItem = document.createElement("article");
             articleItem.className = "articleClass";
 
@@ -18,19 +18,23 @@ getParks()
             let headerEl = document.createElement("h3");
             let paragraphEl = document.createElement("p");
 
-            //does something
-            headerEl.textContent = "Name : " + parsedValue[i].name;
-            paragraphEl.textContent = "State : " + parsedValue[i].state;
+            //grab data from api by key and concatenates element
+            headerEl.textContent = "Name : " + natlParks[i].name;
+            paragraphEl.textContent = "State : " + natlParks[i].state;
 
+            //appends value from api to DOM.
             articleItem.appendChild(headerEl);
-            articleItem.appendChild(paragraphEl)
+            articleItem.appendChild(paragraphEl);
 
-            if (parsedValue[i].visited){
+            //checks to see if national park has been visited or not.
+            if (natlParks[i].visited) {
                 articleItem.classList.add("visited");
             } else {
                 articleItem.classList.add("notVisited");
             }
 
+
+            //targets display-container
             container.appendChild(articleItem)
         }
 
